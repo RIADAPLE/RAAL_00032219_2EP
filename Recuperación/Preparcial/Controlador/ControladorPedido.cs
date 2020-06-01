463d84b6123ad415675b977742a72d1cb66b59f5
RIADAPLE <00032219@uca.edu.sv>
2020-05-31 19:12:38 -0600

    subiendo la recuperación

---
+++ Recuperación/Preparcial/Controlador/ControladorPedido.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using Preparcial.Modelo;
+
+namespace Preparcial.Controlador
+{
+    public static class ControladorPedido
+    {
+        public static DataTable GetPedidosUsuarioTable(string id)
+        {
+            
+            DataTable pedidos = null;
+
+            try
+            {
+                pedidos = ConexionBD.EjecutarConsulta("SELECT p.idPedido, i.nombreart, p.cantidad, i.precio, (i.precio * p.cantidad) AS total" +
+                                            " FROM PEDIDO p, INVENTARIO i, USUARIO u" +
+                                            " WHERE p.idArticulo = i.idArticulo" +
+                                            " AND p.idUsuario = u.idUsuario" +
+                                            $" AND u.idUsuario = {id}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error");
+            }
+
+            return pedidos;
+        }
+
+        public static DataTable GetPedidosTable()
+        {
+            DataTable pedidos = null;
+
+            try
+            {
+                pedidos = ConexionBD.EjecutarConsulta("SELECT p.idPedido, i.nombreArt, p.cantidad, i.precio, (i.precio * p.cantidad) AS total" +
+                                            " FROM PEDIDO p, INVENTARIO i, USUARIO u" +
+                                            " WHERE p.idArticulo = i.idArticulo" +
+                                            " AND p.idUsuario = u.idUsuario");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error");
+            }
+
+            return pedidos;
+        }
+
+        public static void HacerPedido(string idUsuario, string idArticulo, string cantidad)
+        {
+            try
+            {
+                ConexionBD.EjecutarComando("INSERT INTO PEDIDO(idUsuario, idArticulo, cantidad) " +
+                    $"VALUES({idUsuario}, {idArticulo}, {cantidad})");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error");
+            }
+        }
+    }
+}
